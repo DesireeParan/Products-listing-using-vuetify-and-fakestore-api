489617c1e256ff2d52a54e5240c2d6e152cfe2c5
Desiree Paran <paranmariadesiree@gmail.com>
2025-06-13 01:39:13 +0800

    updates npm to deploy publicly

--- components.d.ts
+++ components.d.ts
@@ -8,14 +8,14 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AddProduct: typeof import('./src/components/AddProduct.vue')['default']
     AddProductModal: typeof import('./src/components/AddProductModal.vue')['default']
     EditProductModal: typeof import('./src/components/EditProductModal.vue')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     ProductDetailsModal: typeof import('./src/components/ProductDetailsModal.vue')['default']
     ProductList: typeof import('./src/components/ProductList.vue')['default']
     ProductMgmt: typeof import('./src/components/ProductMgmt.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
     Welcome: typeof import('./src/components/Welcome.vue')['default']
   }
 }
